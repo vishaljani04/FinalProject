0d09f1e23af683d4cb9da5e8ad268f9b47569435
ALLISWELL09 <102709879+ALLISWELL09@users.noreply.github.com>
2023-03-12 11:50:09 +0530

    Add files via upload

---
+++ login_config.php
@@ -0,0 +1,56 @@
+<?php 
+session_start(); 
+include "config.php";
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+}
+	$email_id = validate($_POST['email']);
+	$pass = validate($_POST['password']);
+
+	if (empty($email_id)) {
+		header("Location: index.php?error=Email Id is required");
+	    exit();
+	}
+	else if(empty($pass)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}
+	
+	
+		$sql = "SELECT * FROM login WHERE email='$email_id' AND password='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+
+		if (mysqli_num_rows($result) === 1) {
+			// $row = mysqli_fetch_assoc($result);
+            // if ($row['email'] === $email_id && $row['password'] === $pass) {
+            // 	$_SESSION['email'] = $row['email'];
+            // 	// $_SESSION['name'] = $row['name'];
+            // 	$_SESSION['id'] = $row['id']; 
+            	header("Location: home.php");
+		        exit();
+			
+		
+
+		
+            }
+			else{
+				header("Location: index.php?error=Incorect User name or password");
+		        exit();
+			}
+		
+		// }
+
+		// else{
+		// 	header("Location: index.php");
+	    //     exit();
+		// }
+
+	
